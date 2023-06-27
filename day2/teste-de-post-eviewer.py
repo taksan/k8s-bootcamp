d34f894177977368e7b9547f8169a3a727ea7cf7
Takeuchi <g.takeuchi@gmail.com>
2023-06-26 22:02:30 -0300

    Reorganization

--- day2/teste-de-post-eviewer.py
+++ day2/teste-de-post-eviewer.py
@@ -1,6 +1,6 @@
 import requests
 
 # troque o IP abaixo pelo IP do event viewer
-target="http://192.168.162.138:5000/api/events"
+target="http://192.168.162.169:5000/api/events"
 message = {"environment": "foo", "content": "bar"}
 requests.post(url=target, json=message)
